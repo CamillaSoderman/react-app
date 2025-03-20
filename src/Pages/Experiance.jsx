a3f3bed172eeee0947034c210d000818126412a6
Camilla <camilla.e.soderman@gmail.com>
2025-03-20 13:58:54 +0100

    try 1000

--- src/Pages/Experiance.jsx
+++ src/Pages/Experiance.jsx
@@ -9,20 +9,7 @@ export default function Experiance(){
     return (
         <>
         <h1>Experiance</h1>
-        <CardGroup>
-            {Cv.experiance.map( (experiance) => {
-              return (
-              <Card>
-                <Card.Img variant="top" src="https://picsum.photos/200/300" />
-                <Card.Body>
-                  <Card.Title>{experiance.Company}</Card.Title>
-                  <Card.Text>{experiance.Year}</Card.Text>
-                  <Card.Text>{experiance.Description}</Card.Text>
-                </Card.Body>
-              </Card>
-              )
-            })}
-        </CardGroup>
+
         </>
       )
     
