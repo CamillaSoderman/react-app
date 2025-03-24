486a8812b01c174167c7d1eaaa9792ae496bbd48
Camilla <camilla.e.soderman@gmail.com>
2025-03-24 10:38:14 +0100

    hashrouter added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,15 +4,19 @@ import proPic from '../assets/img/profilepic.png'
 export default function About() {
     return (
         <><h1>About Me</h1>
-        <img id="aboutMePic" src={proPic} alt="image of Camilla"></img>
-            <p>Hi! I’m Camilla, a passionate tech enthusiast with a background in ABAP programming,
-                now diving into the world of full-stack .NET development. 
-                When I’m not coding, you’ll probably find me baking something delicious, 
-                leveling up in my favorite video games, catching up with friends, or hitting the gym. 
-                I love the balance between creativity, logic, and staying active—whether it’s in the kitchen, at my desk, or on the weight rack.
-                Let’s connect and create something awesome together!
+            <div className="about_me_content">
+                <img id="aboutMePic" src={proPic} alt="image of Camilla" />
 
-            </p>
+                <div id="about_me_p">
+                    <p>Hi! I’m Camilla, a passionate tech enthusiast with a background in ABAP programming,
+                        now diving into the world of full-stack .NET development.
+                        When I’m not coding, you’ll probably find me baking something delicious,
+                        leveling up in my favorite video games, catching up with friends, or hitting the gym.
+                        I love the balance between creativity, logic, and staying active—whether it’s in the kitchen, at my desk, or on the weight rack.
+                        Let’s connect and create something awesome together!
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './Style/index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
