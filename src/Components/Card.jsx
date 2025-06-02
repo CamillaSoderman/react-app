d5c9803fe3255d2e2bb398468fdf0a46c9a39a0a
Camilla <camilla.e.soderman@gmail.com>
2025-06-02 10:16:53 +0200

    updated button on index to about

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,61 +1,63 @@
-import chaschingImg from '../assets/img/chasching.png'
-import proPic from '../assets/img/profilepic.png'
-import linkedIn from '../assets/img/linkedinicon.png'
-import '../Style/index.css'
-
-
+import { useState } from 'react';
+import proPic from '../assets/img/profilepic.png';
+import chaschingImg from '../assets/img/chasching.png';
+import linkedIn from '../assets/img/linkedinicon.png';
+import '../Style/index.css';
 
 export default function Card() {
+  const [openPopover, setOpenPopover] = useState(null);
 
-    return (
-        <>
-            <div className="project-section">
-                <article className="card-index">
-                    <div className="card-head-index">
-                        <img src={proPic} alt="image of Camilla" className="card-img-fixed" />
+  return (
+    <div className="project-section">
+      {/* Card 1 */}
+      <article className="card-index">
+        <div className="card-head-index">
+          <img src={proPic} alt="image of Camilla" className="card-img-fixed" />
+        </div>
+        <div className="card-body-index">
+          <h3>Camilla</h3>
+          <p>This is me, Hi!</p>
+        </div>
 
-                    </div>
-                    <div className="card-body-index">
-                        <h3>Camilla</h3>
-                        <p>This is me, Hi!</p>
-                    </div>
+        <button onClick={() => setOpenPopover(openPopover === 1 ? null : 1)}>More info</button>
+        {openPopover === 1 && (
+          <div className="popover-box">
+            <h2>More info</h2>
+            <p>You can find more information about me under the tab "About"</p>
+          </div>
+        )}
+      </article>
 
-                    <button popoverTarget="moreinfo1">More info</button>
-                    <div id="moreinfo1" popover="auto">
-                        <h2>More info</h2>
-                        <p>You can find more information about me under the tab "About"</p>
-                    </div>
-                </article>
-                <article className="card-index">
-                    <div className="card-head-index">
-                        <img src={chaschingImg} alt="image of project Chas-ching" className="card-img-fixed" />
+      {/* Card 2 */}
+      <article className="card-index">
+        <div className="card-head-index">
+          <img src={chaschingImg} alt="image of project Chas-ching" className="card-img-fixed" />
+        </div>
+        <div className="card-body-index">
+          <h3>Latest Project</h3>
+          <p>Chas-Ching</p>
+        </div>
 
-                    </div>
-                    <div className="card-body-index">
-                        <h3>Latest Project</h3>
-                        <p>Chas-Ching</p>
-                    </div>
-                    <button popoverTarget="moreinfo2">More info</button>
-                    <div id="moreinfo2" popover="auto">
-                        <h2>More info</h2>
-                        <p>You can find more information about me under the tab "Projects"</p>
-                    </div>
-                </article>
-                <article className="card-index">
-                    <div className="card-head-index">
-                        <img src={linkedIn} alt="linked in icon" className="card-img-fixed" />
+        <button onClick={() => setOpenPopover(openPopover === 2 ? null : 2)}>More info</button>
+        {openPopover === 2 && (
+          <div className="popover-box">
+            <h2>More info</h2>
+            <p>You can find more information about me under the tab "Projects"</p>
+          </div>
+        )}
+      </article>
 
-                    </div>
-                    <div className="card-body-index">
-                        <h3></h3>
-                        <a href="https://www.linkedin.com/in/camilla-s%C3%B6derman-458098157/"><h3>Lets Connect</h3></a>
-                    </div>
-                </article>
-            </div>
-        </>   
-
-
-    );
+      {/* Card 3 */}
+      <article className="card-index">
+        <div className="card-head-index">
+          <img src={linkedIn} alt="linked in icon" className="card-img-fixed" />
+        </div>
+        <div className="card-body-index">
+          <a href="https://www.linkedin.com/in/camilla-s%C3%B6derman-458098157/">
+            <h3>Let's Connect</h3>
+          </a>
+        </div>
+      </article>
+    </div>
+  );
 }
-
-
